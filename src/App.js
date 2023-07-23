53e37f0d63355d26c4b93a609dfe043452f1426a
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-23 22:00:43 +0200

    update ui✅

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ import NFT from "./abis/NFT.json";
 
 // Config
 import config from "./config.json";
-import { url } from "inspector";
 
 function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
-  const [url, setURL] = useState(null);
+  const [nft, setNFT] = useState(null);
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
   const [url, setURL] = useState(null);
 
-  const [isWaiting, setWaiting] = useState(false)
+  const [message, setMessage] = useState("");
+  const [isWaiting, setIsWaiting] = useState(false);
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -49,19 +49,23 @@ function App() {
       return;
     }
 
-    setIsWaiting(true)
+    setIsWaiting(true);
 
-    const imageData = createImage();
+    // Call AI API to generate a image based on description
+    const imageData = await createImage();
 
+    // Upload image to IPFS (NFT.Storage)
     const url = await uploadImage(imageData);
 
+    // Mint NFT
     await mintImage(url);
 
-    setIsWaiting(false)
+    setIsWaiting(false);
+    setMessage("");
   };
 
   const createImage = async () => {
-    console.log("Generate Image...");
+    setMessage("Generating Image...");
 
     // You can replace this with different model API's
     const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
@@ -86,14 +90,16 @@ function App() {
     const data = response.data;
 
     const base64data = Buffer.from(data).toString("base64");
-    const img = "data:${type};base64," + base64data;
+    const img = `data:${type};base64,` + base64data; // <-- This is so we can render it on the page
     setImage(img);
 
     return data;
   };
-  const uploadImage = async (imageData) => {
-    console.log("Uploading Image...");
 
+  const uploadImage = async (imageData) => {
+    setMessage("Uploading Image...");
+
+    // Create instance to NFT.Storage
     const nftstorage = new NFTStorage({
       token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
     });
@@ -111,57 +117,66 @@ function App() {
 
     return url;
   };
-}
-const mintImage = async (tokenURI) => {
-  console.log("Waiting for Mint...");
 
-  const signer = await provider.getSigner();
-  const transaction = await nft
-    .connect(signer)
-    .mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") });
-  await transaction.wait();
-};
+  const mintImage = async (tokenURI) => {
+    setMessage("Waiting for Mint...");
 
-useEffect(() => {
-  loadBlockchainData();
-}, []);
+    const signer = await provider.getSigner();
+    const transaction = await nft
+      .connect(signer)
+      .mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") });
+    await transaction.wait();
+  };
 
-return (
-  <div>
-    <Navigation account={account} setAccount={setAccount} />
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
-    <div className="form">
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          placeholder="Create a name..."
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Create a description..."
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <input type="submit" value="Create & Mint" />
-      </form>
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
 
-      <div className="image">
-        <img src={image} alt="AI generated Image"/>
-        <div className="image_placeholder">
-          <spinner animation="border" />
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Create a name..."
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Create a description..."
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input type="submit" value="Create & Mint" />
+        </form>
+
+        <div className="image">
+          {!isWaiting && image ? (
+            <img src={image} alt="AI generated image" />
+          ) : isWaiting ? (
+            <div className="image__placeholder">
+              <Spinner animation="border" />
+              <p>{message}</p>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
-    </div>
 
-    <p>
-      View&nbsp;
-      <a href={url} target="_blank" rel="noreferrer">
-        Metadata
-      </a>
-    </p>
-  </div>
-);
+      {!isWaiting && url && (
+        <p>
+          View&nbsp;
+          <a href={url} target="_blank" rel="noreferrer">
+            Metadata
+          </a>
+        </p>
+      )}
+    </div>
+  );
+}
 
 export default App;
