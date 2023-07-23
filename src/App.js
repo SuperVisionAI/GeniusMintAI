760469e1329b59d691c6434b3f0640445127f4a0
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-23 21:50:15 +0200

    update ui✅

--- src/App.js
+++ src/App.js
@@ -18,117 +18,144 @@ import { url } from "inspector";
 function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
+  const [url, setURL] = useState(null);
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-  const [image, setImage] = useState(null)
-  const [url, setURL] = useState(null)
-
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+  const [url, setURL] = useState(null);
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    const nft = new ethers.Contract(
+      config[network.chainId].nft.address,
+      NFT,
+      provider
+    );
+    setNFT(nft);
   };
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const imageData = createImage()
+    if (name === "" || description === "") {
+      window.alert("Please provide a name and description");
+      return;
+    }
 
-    const url = await uploadImage(imageData)
+    const imageData = createImage();
 
-    console.log("url", url)
+    const url = await uploadImage(imageData);
+
+    await mintImage(url);
   };
 
-  const createImage = async ( ) => {
-    console.log("Generate Image...")
-
+  const createImage = async () => {
+    console.log("Generate Image...");
 
     // You can replace this with different model API's
-    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`
+    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
 
-       // Send the request
-       const response = await axios({
-        url: URL,
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        data: JSON.stringify({
-          inputs: description, options: { wait_for_model: true },
-        }),
-        responseType: 'arraybuffer',
-      })
+    // Send the request
+    const response = await axios({
+      url: URL,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        inputs: description,
+        options: { wait_for_model: true },
+      }),
+      responseType: "arraybuffer",
+    });
 
-      const type = response.headers['content-type']
-      const data = response.data
+    const type = response.headers["content-type"];
+    const data = response.data;
 
-      const base64data = Buffer.from(data).toString('base64')
-      const img = 'data:${type};base64,' + base64data
-      setImage(img)
+    const base64data = Buffer.from(data).toString("base64");
+    const img = "data:${type};base64," + base64data;
+    setImage(img);
 
-      return data
-    }
-    const uploadImage = async (imageData) => {
-      console.log("Uploading Image...")
+    return data;
+  };
+  const uploadImage = async (imageData) => {
+    console.log("Uploading Image...");
 
-      const nftstorage = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_API_KEY})
+    const nftstorage = new NFTStorage({
+      token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
+    });
 
-      // Send request to store image
+    // Send request to store image
     const { ipnft } = await nftstorage.store({
       image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
       name: name,
       description: description,
-    })
+    });
 
-      // Save the URL
-      const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`
-      setURL(url)
-  
-      return url
-    }
-    }
+    // Save the URL
+    const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
+    setURL(url);
 
-  useEffect(() => {
-    loadBlockchainData();
-  }, []);
+    return url;
+  };
+}
+const mintImage = async (tokenURI) => {
+  console.log("Waiting for Mint...");
 
-  return (
-    <div>
-      <Navigation account={account} setAccount={setAccount} />
+  const signer = await provider.getSigner();
+  const transaction = await nft
+    .connect(signer)
+    .mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") });
+  await transaction.wait();
+};
 
-      <div className="form">
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            placeholder="Create a name..."
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Create a description..."
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <input type="submit" value="Create & Mint" />
-        </form>
+useEffect(() => {
+  loadBlockchainData();
+}, []);
 
-        <div className="image">
-          <img src={image} alt="AI generated Image"></img>
+return (
+  <div>
+    <Navigation account={account} setAccount={setAccount} />
+
+    <div className="form">
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Create a name..."
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Create a description..."
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input type="submit" value="Create & Mint" />
+      </form>
+
+      <div className="image">
+        <img src={image} alt="AI generated Image"/>
+        <div className="image_placeholder">
+          <spinner animation="border" />
         </div>
       </div>
-
-      <p>
-        View&nbsp;
-        <a href={url} target="_blank" rel="noreferrer">
-          Metadata
-        </a>
-      </p>
     </div>
-  );
 
+    <p>
+      View&nbsp;
+      <a href={url} target="_blank" rel="noreferrer">
+        Metadata
+      </a>
+    </p>
+  </div>
+);
 
 export default App;
